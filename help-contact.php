2de6823483723d6a7a755afe261931c8a3d14944
kritriv <vishalsaraiwal68@gmail.com>
2023-11-09 12:57:30 +0530

    contact form added

--- help-contact.php
+++ help-contact.php
@@ -1,11 +1,31 @@
 <?php include_once 'components/navbar.php'  ?>
 
 <div class="container-fluid">
-        <div class="card">
-          <div class="card-body">
-            <h5 class="card-title fw-semibold mb-4">Help & Contact</h5>
-            <p class="mb-0">This is a sample page </p>
-          </div>
+        <div class="">
+        <div class="card-body">
+              <h5 class="card-title fw-semibold mb-4">Need Any Help?</h5>
+              <div class="card">
+                <div class="card-body">
+                  <form>
+                    <div class="mb-3">
+                      <label for="Contact_email" class="form-label">Email address</label>
+                      <input type="email" class="form-control" id="Contact_email" aria-describedby="emailHelp" placeholder="xyz@gmail.com">
+                      <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                    </div>
+                    <div class="mb-3">
+                      <label for="Contact_number" class="form-label">Enter Contact No.</label>
+                      <input type="number" class="form-control" id="Contact_number" aria-describedby="emailHelp" placeholder="+1 189462030">
+                    </div>
+                    <div class="mb-5">
+                      <label for="Contact_Message" class="form-label">Enter your Queries</label>
+                      <textarea class="form-control" name="Contact_Message" id="Contact_Message" cols="30" rows="8" placeholder="Type Query Message ..."></textarea>
+                    </div>
+                    
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                  </form>
+                </div>
+              </div>
+            </div>
         </div>
       </div>
 
